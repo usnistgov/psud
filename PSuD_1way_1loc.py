78cf4102a84f6a3017b694693a437b54231284c2
Jesse <jesse.frey@nist.gov>
2020-10-02 11:04:55 -0600

    Initial commit with skeleton code

---
+++ PSuD_1way_1loc.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+if __name__ == "__main__":
+    from radioInterface import RadioInterface
+    
+
+
+class PSuD:
+    
+    def __init__(self):
+        #set default values
+        self.audioFiles=[]
+        self.overPlay=1.0
+        self.trials=100
+        self.blockSize=512
+        self.bufSize=20
+        self.ri=None
+        self.info=None
+        
+    def run(self):
+        #--------------------------[Load Audio Files]--------------------------
+        #-------------------[Find and Setup Audio interface]-------------------
+        #-----------------------[Setup Files and folders]-----------------------
+        #---------------------------[write log entry]---------------------------
+        
+        #--------------------------[Measurement Loop]--------------------------
+        for trial in range(self.trials):
+            #-----------------------[Get Trial Timestamp]-----------------------
+            #--------------------[Key Radio and play audio]--------------------
+            #-----------------------[Pause Between runs]-----------------------
+            #------------------------[calculate M2E]------------------------
+            #---------------------[Compute intelligibility]---------------------
+            pass
+
+
+
+#main function 
+if __name__ == "__main__":
+
+    #---------------------------[Create Test object]---------------------------
+
+    #create object here to use default values for arguments
+    test_obj=PSuD()
+
+    #-----------------------[Setup ArgumentParser object]-----------------------
+
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+    parser.add_argument(
+                        '-a', '--audioFiles', default=[],action="extend", nargs="+", type=str,
+                        help='Choose audiofiles to use for test.')
+    parser.add_argument('-t', '--trials', type=int, default=test_obj.trials,
+                        help='Number of trials to use for test. Defaults to %(default)')
+    parser.add_argument("-r", "--radioport", default="",
+                        help="Port to use for radio interface. Defaults to the first"+
+                        " port where a radio interface is detected")
+    parser.add_argument('-b', '--blockSize', type=int, default=test_obj.blockSize,
+                        help='Block size for transmitting audio (default: %(default)s)')
+    parser.add_argument('-q', '--bufferSize', type=int, default=test_obj.bufSize,metavar='bufSize',
+                        help='Number of blocks used for buffering audio (default: %(default)s)')
+    parser.add_argument('-p', '--overPlay', type=float, default=test_obj.overPlay, 
+                        help='The number of seconds to play silence after the audio is complete'+
+                        '. This allows for all of the audio to be recorded when there is delay'+
+                        ' in the system')
+    parser.add_argument('-o', '--outdir', default='', help='Directory that is added to the '+
+                        'output path for all files')
+                        
+                        
+    #-----------------------------[Parse arguments]-----------------------------
+
+    args = parser.parse_args()
+    
+    #set object properties that exist
+    for k,v in vars(args).items():
+        if hasattr(test_obj,k):
+            setattr(test_obj,k,v)
+            
+    #------------------------------[Get test info]------------------------------
+    #---------------------------[Open RadioInterface]---------------------------
+    
+    with RadioInterface(args.radioport) as test_obj.ri:
+        #------------------------------[Run Test]------------------------------
+        test_obj.run()
+        
