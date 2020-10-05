b8dc095ab775b479a40c304fdfc0ff1b93736cfc
Jesse <jesse.frey@nist.gov>
2020-10-05 13:02:27 -0600

    Update arguments
    fix some errors with using defaults in the help text and add metavar
    arguments

--- PSuD_1way_1loc.py
+++ PSuD_1way_1loc.py
@@ -50,24 +50,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__)
     parser.add_argument(
-                        '-a', '--audioFiles', default=[],action="extend", nargs="+", type=str,
-                        help='Choose audiofiles to use for test.')
-    parser.add_argument('-t', '--trials', type=int, default=test_obj.trials,
-                        help='Number of trials to use for test. Defaults to %(default)')
-    parser.add_argument("-r", "--radioport", default="",
+                        '-a', '--audioFiles', default=[],action="extend", nargs="+", type=str,metavar='FILENAME',
+                        help='Path to audio files to use for test. Cutpoint files must also be present')
+    parser.add_argument('-t', '--trials', type=int, default=test_obj.trials,metavar='T',
+                        help='Number of trials to use for test. Defaults to %(default)d')
+    parser.add_argument("-r", "--radioport", default="",metavar='PORT',
                         help="Port to use for radio interface. Defaults to the first"+
                         " port where a radio interface is detected")
-    parser.add_argument('-b', '--blockSize', type=int, default=test_obj.blockSize,
-                        help='Block size for transmitting audio (default: %(default)s)')
-    parser.add_argument('-q', '--bufferSize', type=int, default=test_obj.bufSize,metavar='bufSize',
-                        help='Number of blocks used for buffering audio (default: %(default)s)')
-    parser.add_argument('-p', '--overPlay', type=float, default=test_obj.overPlay, 
+    parser.add_argument('-b', '--blockSize', type=int, default=test_obj.blockSize,metavar='SZ',
+                        help='Block size for transmitting audio (default: %(default)d)')
+    parser.add_argument('-q', '--bufferSize', type=int, default=test_obj.bufSize,dest='bufSize',metavar='SZ',
+                        help='Number of blocks used for buffering audio (default: %(default)d)')
+    parser.add_argument('-p', '--overPlay', type=float, default=test_obj.overPlay,metavar='DUR',
                         help='The number of seconds to play silence after the audio is complete'+
                         '. This allows for all of the audio to be recorded when there is delay'+
                         ' in the system')
-    parser.add_argument('-o', '--outdir', default='', help='Directory that is added to the '+
-                        'output path for all files')
-                        
+    parser.add_argument('-o', '--outdir', default='', metavar='DIR',
+                        help='Directory that is added to the output path for all files')
+                            
                         
     #-----------------------------[Parse arguments]-----------------------------
 
