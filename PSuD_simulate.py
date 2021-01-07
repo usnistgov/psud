f532b33b2d9601f834a3d7d927f1cc980f4977a1
Jesse <jesse.frey@nist.gov>
2021-01-07 16:20:44 -0700

    Fix spelling of channel

--- PSuD_simulate.py
+++ PSuD_simulate.py
@@ -73,9 +73,9 @@ if __name__ == "__main__":
                         help='Time interval for probabilityiesr in seconds')
     parser.add_argument('--m2e-min-corr', default=test_obj.m2e_min_corr, metavar='C',dest='m2e_min_corr',
                         help='Minimum correlation value for acceptable mouth 2 ear measurement (default: %(default)0.2f)')
-    parser.add_argument('-c','--channel-tech', default=sim_obj.chanel_tech, metavar='TECH',dest='chanel_tech',
+    parser.add_argument('-c','--channel-tech', default=sim_obj.channel_tech, metavar='TECH',dest='channel_tech',
                         help='Channel technology to simulate (default: %(default)s)')
-    parser.add_argument('--channel-rate', default=sim_obj.chanel_rate, metavar='RATE',dest='chanel_rate',
+    parser.add_argument('--channel-rate', default=sim_obj.channel_rate, metavar='RATE',dest='channel_rate',
                         help='Channel technology rate to simulate. Passing \'None\' will use the technology default. (default: %(default)s)')
     parser.add_argument('--channel-m2e', type=float, default=sim_obj.m2e_latency, metavar='L',dest='m2e_latency',
                         help='Channel mouth to ear latency, in seconds, to simulate. (default: %(default)s)')
@@ -102,13 +102,13 @@ if __name__ == "__main__":
     
     #-------------------------[Set simulation settings]-------------------------
 
-    sim_obj.chanel_tech=args.chanel_tech
+    sim_obj.channel_tech=args.channel_tech
     
-    #set chanel rate, check for None
-    if(args.chanel_rate=='None'):
-        sim_obj.chanel_rate=None
+    #set channel rate, check for None
+    if(args.channel_rate=='None'):
+        sim_obj.channel_rate=None
     else:
-        sim_obj.chanel_rate=args.chanel_rate
+        sim_obj.channel_rate=args.channel_rate
         
     sim_obj.m2e_latency=args.m2e_latency
 
