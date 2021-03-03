1a4ca9401e05a02094535f449a6837f4400f10cc
Jaden <jaden.pieper@nist.gov>
2021-03-02 17:03:09 -0700

    Messed around with lots of plot things, very messy

--- sweep_methods.py
+++ sweep_methods.py
@@ -9,22 +9,26 @@ import pdb
 import itertools
 import os
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotnine as pn
 import datetime
 
 
 
 from itertools import combinations
 from matplotlib.legend_handler import HandlerLine2D
+# from plotnine import ggplot, geom_point, geom_line, aes
+
 from PSuD_eval import evaluate
 
 def sweep(t_proc):
-    methods = ["EWC","ARF"]
+    methods = ["EWC","ARF", "AMI"]
     thresholds = [0.5, 0.7, 0.9]
     lengths = [1, 3, 5, 10]
-    weights = np.arange(0.5,0.9,0.1)
+    weights = np.arange(0.1,0.91,0.1)
     
     results = pd.DataFrame(columns = ['Method','Weight','Threshold','Length','PSuD','PSuD_LB','PSuD_UB'])
     
@@ -34,7 +38,7 @@ def sweep(t_proc):
         
         for t in thresholds:
             print('Method: {}, Threshold: {}'.format(method,t))
-            if(method == "EWC"):
+            if(method == "EWC" or method == "AMI"):
                 for mlen in lengths:
                     psud_m,psud_ci = t_proc.eval_psud(t,
                                                       mlen,
@@ -62,49 +66,88 @@ def plot_sweep(results,outpath):
     thresholds = np.unique(results['Threshold'])
     
     marker = itertools.cycle(('+', 'v', 'o', '*')) 
-
+    
     ncol = len(lengths)
-    fig, ax = plt.subplots(2,ncol,figsize=(40,20))  # Create a figure and an axes.
+    fig, ax = plt.subplots(1,ncol,figsize=(40,20))  # Create a figure and an axes.
     
     for pix,mlen in enumerate(lengths):
         plots = []
         
         # First Plot
+        color = itertools.cycle(("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"))
         for method in methods:
+            mcolor = next(color)
             for thresh in thresholds:    
                 fdf = results[(results['Method'] == method) & (results['Length'] == mlen) & (results['Threshold'] == thresh)]
                 label_str = '{} - {}'.format(method, thresh)
                 if(method == "EWC"):
-                    ax[0][pix].axhline(y=fdf['PSuD'].iloc[0],color = 'black',marker = next(marker), linestyle = 'dashed',label=label_str)
+                    ax[pix].axhline(y=fdf['PSuD'].iloc[0],color = 'black',marker = next(marker), linestyle = 'dashed',label=label_str)
+                elif(method == "AMI"):
+                    ax[pix].axhline(y=fdf['PSuD'].iloc[0],color = mcolor,marker = next(marker),label=label_str)
                 else:
-                    ax[0][pix].plot(fdf['Weight'], fdf['PSuD'],label=label_str,marker = next(marker))
+                    ax[pix].plot(fdf['Weight'], fdf['PSuD'],label=label_str,marker = next(marker),color=mcolor)
         
-        # Second Plot
-        for method in methods:
-            if(method == "EWC"):
-                # Make second plot
-                fdf = results.loc[(results['Method'] == method) & (results['Length'] == mlen)]
-                label_str = 'EWC'
-                ax[1][pix].plot(fdf['Threshold'],fdf['PSuD'], label= label_str, marker= next(marker),color='black')
+        # # Second Plot
+        # for method in methods:
+        #     if(method == "EWC"):
+        #         # Make second plot
+        #         fdf = results.loc[(results['Method'] == method) & (results['Length'] == mlen)]
+        #         label_str = 'EWC'
+        #         ax[1][pix].plot(fdf['Threshold'],fdf['PSuD'], label= label_str, marker= next(marker),color='black')
                 
-            else:
-                for w in weights:
-                    label_str = '{} - weight {}'.format(method,np.round(w,2))
-                    fdf = results.loc[(results['Method'] == method) & (results['Length'] == mlen) & (results['Weight'] == w) ]
-                    ax[1][pix].plot(fdf['Threshold'],fdf['PSuD'],label=label_str, marker= next(marker))
+        #     else:
+        #         for w in weights:
+        #             label_str = '{} - weight {}'.format(method,np.round(w,2))
+        #             fdf = results.loc[(results['Method'] == method) & (results['Length'] == mlen) & (results['Weight'] == w) ]
+        #             ax[1][pix].plot(fdf['Threshold'],fdf['PSuD'],label=label_str, marker= next(marker))
                     
-        ax[0][pix].set_title('PSuD({}) Param Sweep'.format(mlen))
-        ax[0][pix].set_xlabel('Method Weight')
-        ax[0][pix].set_ylabel('PSuD')
-        ax[0][pix].legend()
+        ax[pix].set_ylim([0,1])
+        ax[pix].grid()
+        ax[pix].set_title('PSuD({}) Param Sweep'.format(mlen))
+        ax[pix].set_xlabel('Method Weight')
+        ax[pix].set_ylabel('PSuD')
+        ax[pix].legend()
         
-        ax[1][pix].set_xlabel('Intelligibility Success Threshold')
-        ax[1][pix].set_ylabel('PSuD')
-        ax[1][pix].legend()
-        plt.savefig(outpath)
+        # ax[1][pix].set_xlabel('Intelligibility Success Threshold')
+        # ax[1][pix].set_ylabel('PSuD')
+        # ax[1][pix].legend()
+    plt.savefig(outpath)
         # plt.legend(handler_map={plots[0]: HandlerLine2D(numpoints=4)})
     
-
+def new_plot(results):
+    lengths = np.unique(results['Length'])
+    methods = np.unique(results['Method'])
+    # Code for one big messy plot
+    # p = (pn.ggplot(results, pn.aes('Weight', 'PSuD', shape='factor(Threshold)',color="factor(Length)"))
+    #      + pn.geom_line()
+    #      + pn.geom_point()
+    #      +pn.theme_light())
+    # ewc = results[results['Method'] == 'EWC']
+    # for rix,tval in ewc.iterrows():
+    #     p = (p+
+    #          pn.geom_hline(yintercept=tval['PSuD'],linetype='dashed'))
+    # print(p)
+    plots = []
+    for mlen in lengths:
+        mdf = results.loc[(results['Length'] == mlen)]
+        fdf = mdf[(mdf['Method'] != "EWC")]
+        p = (pn.ggplot(fdf, pn.aes('Weight', 'PSuD', shape='factor(Threshold)',color="Method"))
+         + pn.geom_line()
+         + pn.geom_point()
+         +pn.theme_light()
+         +pn.scale_y_continuous(breaks=np.arange(0,1,0.2),
+                                minor_breaks=np.arange(0,1,0.1),
+                                limits = [0,1]))
+        ewc = mdf[mdf['Method'] == "EWC"]
+        for rix,tval in ewc.iterrows():
+            p = (p+
+                 pn.geom_hline(yintercept=tval['PSuD'],linetype='dashed'))
+        # print(p)
+        p = (p +
+             pn.ggtitle('Message Length {} s'.format(mlen)))
+        plots.append(p)
+    print(plots)
+        
 #--------------------------[main]----------------------------------------------
 if(__name__ == "__main__"):
         # Set up argument parser
@@ -154,13 +197,13 @@ if(__name__ == "__main__"):
     #TODO: Can we call R from python?
                         
                 
-                    
+#--------------------------------[Easy reruns]--------------------------------                    
                 
 # Test Analog direct:
-# runfile('D:/MCV_671DRDOG/psud/PSuD_eval.py', wdir='D:/MCV_671DRDOG/psud', args = 'Rcapture_Analog-direct_11-Feb-2021_14-23-10 Rcapture_Analog-direct_11-Feb-2021_11-45-21 Rcapture_Analog-direct_11-Feb-2021_09-51-59 Rcapture_Analog-direct_11-Feb-2021_06-22-06 -p data -m 1 3 5 10 -t 0.5 0.7 0.9 --method ARF')
+# runfile('D:/MCV_671DRDOG/psud/sweep_methods.py', wdir='D:/MCV_671DRDOG/psud', args = 'Rcapture_Analog-direct_11-Feb-2021_14-23-10 Rcapture_Analog-direct_11-Feb-2021_11-45-21 Rcapture_Analog-direct_11-Feb-2021_09-51-59 Rcapture_Analog-direct_11-Feb-2021_06-22-06 -p data -o sweeps/analogdirect')
 
 # Test P25 Direct
-# runfile('D:/MCV_671DRDOG/psud/PSuD_eval.py', wdir='D:/MCV_671DRDOG/psud', args = 'capture_P25-direct_22-Feb-2021_14-18-29 capture_P25-direct_22-Feb-2021_12-24-20 capture_P25-direct_22-Feb-2021_10-02-11 capture_P25-direct_23-Feb-2021_07-01-38 -p data -m 1 3 5 10 -t 0.5 0.7 0.9 --method ARF')
+# runfile('D:/MCV_671DRDOG/psud/sweep_methods.py', wdir='D:/MCV_671DRDOG/psud', args = 'capture_P25-direct_22-Feb-2021_14-18-29 capture_P25-direct_22-Feb-2021_12-24-20 capture_P25-direct_22-Feb-2021_10-02-11 capture_P25-direct_23-Feb-2021_07-01-38 -p data -o sweeps/P25direct')
 
 # Test P25 Trunked Phase 1
-# runfile('D:/MCV_671DRDOG/psud/sweep_methods.py', wdir='D:/MCV_671DRDOG/psud', args = 'capture_P25-Trunked-p1_23-Feb-2021_11-28-27 capture_P25-Trunked-p1_23-Feb-2021_09-03-52 capture_P25-Trunked-p1_24-Feb-2021_07-16-10 capture_P25-Trunked-p1_23-Feb-2021_13-24-39 -p data -o sweeps/p25direct')
+# runfile('D:/MCV_671DRDOG/psud/sweep_methods.py', wdir='D:/MCV_671DRDOG/psud', args = 'capture_P25-Trunked-p1_23-Feb-2021_11-28-27 capture_P25-Trunked-p1_23-Feb-2021_09-03-52 capture_P25-Trunked-p1_24-Feb-2021_07-16-10 capture_P25-Trunked-p1_23-Feb-2021_13-24-39 -p data -o sweeps/p25TrunkedP1')
